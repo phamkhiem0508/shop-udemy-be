6d45dd4b39787e1148a291cf5074db65cd01f826
Louis Pham <louis@wisecommerce.io>
2023-09-20 16:49:47 +0700

    Update

--- app.js
+++ app.js
@@ -3,71 +3,82 @@ import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 
 const getPrice = async () => {
-  puppeteer.use(StealthPlugin());
-  puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox'],
-    // executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-    // args: [ '--proxy-server=http:/188.132.222.234:8080' ]
-  });
+    puppeteer.use(StealthPlugin());
+    puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+      // executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+      // args: [ '--proxy-server=http:/188.132.222.234:8080' ]
+    });
 
-  await browser.createIncognitoBrowserContext();
+    await browser.createIncognitoBrowserContext();
+    try {
+    const page = await browser.newPage();
 
-  const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on("request", (interceptedRequest) => {
+      if (interceptedRequest.isInterceptResolutionHandled()) return;
+      if (
+        interceptedRequest.url().endsWith(".png") ||
+        interceptedRequest.url().endsWith(".jpg") ||
+        interceptedRequest.url().endsWith(".jpeg") ||
+        interceptedRequest.url().endsWith(".gif") ||
+        // interceptedRequest.url().endsWith(".css") ||
+        interceptedRequest.url().endsWith(".woff") ||
+        interceptedRequest.url().endsWith(".woff2") ||
+        interceptedRequest.url().endsWith(".ttf") ||
+        interceptedRequest.url().endsWith(".svg") ||
+        interceptedRequest.url().endsWith(".ico")
+      )
+        interceptedRequest.abort();
+      else interceptedRequest.continue();
+    });
 
-  await page.setRequestInterception(true);
-  page.on("request", (interceptedRequest) => {
-    if (interceptedRequest.isInterceptResolutionHandled()) return;
-    if (
-      interceptedRequest.url().endsWith(".png") ||
-      interceptedRequest.url().endsWith(".jpg") ||
-      interceptedRequest.url().endsWith(".jpeg") ||
-      interceptedRequest.url().endsWith(".gif") ||
-      interceptedRequest.url().endsWith(".woff") ||
-      interceptedRequest.url().endsWith(".woff2") ||
-      interceptedRequest.url().endsWith(".ttf") ||
-      interceptedRequest.url().endsWith(".svg") ||
-      interceptedRequest.url().endsWith(".ico")
-    )
-      interceptedRequest.abort();
-    else interceptedRequest.continue();
-  });
+    await page.goto(
+      "https://www.udemy.com/course/the-web-developer-bootcamp/?fbclid=IwAR1U9oVPPdseqA4OHkp7HDbLAQL-AHwM1P21sNsJ08CO4eb_qc0RyZd6glM"
+    );
 
-  await page.goto(
-    "https://www.udemy.com/course/the-web-developer-bootcamp/?fbclid=IwAR1U9oVPPdseqA4OHkp7HDbLAQL-AHwM1P21sNsJ08CO4eb_qc0RyZd6glM"
-  );
+    // Set screen size
 
-  // Set screen size
+    await page.waitForSelector("button[data-testid='smart-bar-opt-in-cta']");
 
-  await page.waitForSelector("button[data-testid='smart-bar-opt-in-cta']");
+    const element = await page.$("button[data-testid='smart-bar-opt-in-cta']");
 
-  const element = await page.$("button[data-testid='smart-bar-opt-in-cta']");
+    element.click();
 
-  element.click();
+    await page.waitForNavigation();
 
-  await page.waitForNavigation();
+    await page.waitForSelector(
+      "div[data-purpose='price-text-container'] div[data-purpose='course-price-text'].ud-clp-discount-price + div[data-purpose='original-price-container']"
+    );
 
-  await page.waitForSelector(
-    "div[data-purpose='price-text-container'] div[data-purpose='course-price-text'].ud-clp-discount-price .ud-sr-only + span"
-  );
+    const price = await page.$(
+      "div[data-purpose='price-text-container'] div[data-purpose='course-price-text'].ud-clp-discount-price > .ud-sr-only + span"
+    );
 
-  const price = await page.$(
-    "div[data-purpose='price-text-container'] div[data-purpose='course-price-text'].ud-clp-discount-price .ud-sr-only + span"
-  );
+    // const img  = await page.$eval("div[data-purpose='introduction-asset'] img", (el) => {
+    //   return el
+    // })
 
-  // const img  = await page.$eval("div[data-purpose='introduction-asset'] img", (el) => {
-  //   return el
-  // })
+    // console.log(img)
 
-  // console.log(img)
+    let value = await page.evaluate((el) => el.textContent, price);
 
-  let value = await page.evaluate((el) => el.textContent, price);
+    console.log(value);
 
-  console.log(value);
-
-  await browser.close();
+    await browser.close();
+  }
+   catch (e) {
+    await browser.close(); 
+  }
 };
 
-  getPrice();
+for (let i = 0; i < 20; i++) {
+  setTimeout(() => {
+    getPrice();
+  }, 1000 * i);
+}
+
+// getPrice()
